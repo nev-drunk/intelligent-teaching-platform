e1d5e5e3112b62b5f861e084e1c3f0563c9dbed5
userName <userEmail>
2026-05-25 14:47:14 +0800

    fix: remove nested git repo

---
+++ backend/src/main/java/com/huadi/intelligentteachingplatform/IntelligentTeachingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.huadi.intelligentteachingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class cIntelligentTeachingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentTeachingPlatformApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/huadi/intelligentteachingplatform/IntelligentTeachingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huadi.intelligentteachingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentTeachingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
